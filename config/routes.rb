25e370093179d6a8e43a4e6c4f2d073b069993bd
ktpeasant <ktpeasant@gmail.com>
2017-02-05 16:45:06 +0000

    add contact page and refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
+  get 'static_pages/contact'
+
   get 'static_pages/about'
 
   get 'static_pages/help'
 
-  root 'application#goodbye'
+  root 'static_pages#home'
 end
